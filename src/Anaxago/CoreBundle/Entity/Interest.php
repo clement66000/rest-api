dbedcddd33a890cab24e7940dd9b50629d7fb12b
clement66000 <clement.soulies@gmail.com>
2018-12-13 11:28:22 +0100

    add interestEntity

--- src/Anaxago/CoreBundle/Entity/Interest.php
+++ src/Anaxago/CoreBundle/Entity/Interest.php
@@ -22,6 +22,61 @@ class Interest
      */
     private $project;
 
+    /**
+     * @var float
+     */
+    private $InvestMoney;
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user): void
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProject()
+    {
+        return $this->project;
+    }
+
+    /**
+     * @param mixed $project
+     */
+    public function setProject($project): void
+    {
+        $this->project = $project;
+    }
+
+    /**
+     * @return float
+     */
+    public function getInvestMoney(): float
+    {
+        return $this->InvestMoney;
+    }
+
+    /**
+     * @param float $InvestMoney
+     */
+    public function setInvestMoney(float $InvestMoney): void
+    {
+        $this->InvestMoney = $InvestMoney;
+    }
+
+
+
 
 }
 
